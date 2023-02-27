1b4298db77569e3d10938c71dad10839be3ef1e8
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-02-27 13:36:46 +0200

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_rev - minic the stdio version of puts
- * @s: the value to process to the stdout in reverse.
+ * print_rev - prints a string, followed by a new line.
+ * @s: input string to print.
  */
 void print_rev(char *s)
 {
-	int count;
+	int i;
 
-	while (*(s + count) != '\0')
+	while (s[i] != '\0')
 	{
-		count++;
+		i++;
 	}
-
-	while (count--)
+	i--;
+	while (s[i] != '\0')
 	{
-		_putchar(*(s + (count)));
+		_putchar(s[i--]);
 	}
 	_putchar('\n');
 }
