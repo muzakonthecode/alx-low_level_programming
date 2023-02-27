cc9ba641369818d9d05f40482377480a248a0141
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-02-27 13:41:48 +0200

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,26 +7,22 @@
  */
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
+	int i, j;
+	char r[1000];
 
-	while (s[i] != 0)
+
+	i = j = 0;
+	while (s[i] != '\0')
 	{
+		r[i] = s[i];
 		i++;
 	}
-
-	length = i;
-	i = 0;
-	j = length - 1;
-
-	while (i < j)
+	i--;
+	while (i >= 0)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
+		s[i] = r[j];
+		i--;
+		j++;
 	}
+	s[j++] = '\0';
 }
