1228cac1d842fce9cb118769c8d6a0ceee5152e8
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-03-06 14:01:52 +0200

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,20 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-while (*haystack != '\0')
-{
-	char *duplicate = haystack;
-
-	while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+	while (*haystack != '\0')
 	{
-		needle++;
+		char *duplicate = haystack;
+
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+		{
+			needle++;
+			haystack++;
+		}
+		if (!*needle != '\0')
+		{
+			return (duplicate);
+		}
 		haystack++;
 	}
-	if (!*needle != '\0')
-	{
-		return (duplicate);
-	}
-	haystack++;
-}
-return (0);
+	return (0);
 }
